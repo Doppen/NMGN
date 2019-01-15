095673f3421832b6bb93fc96aae0fcd609170453
Bas Doppen <basdoppen@gmail.com>
2019-01-15 13:48:19 +0100

    Note content

--- _dist/js/main.js
+++ _dist/js/main.js
@@ -27,3 +27,22 @@ for (var i = 0; i < elems.length; i++) {
 }
 
 document.getElementById("subNavigation").innerHTML = headerNavigation;
+
+var classname = document.querySelectorAll(".noot");
+
+var myFunction = function() {
+    var attribute = this.querySelector("span").getAttribute("id");
+    attribute = attribute.replace("-backlink", "")
+    noteContent = document.getElementById(attribute).innerHTML;
+    document.getElementById("chapterIllustration").innerHTML= noteContent;
+
+    //alert(noteContent);
+
+};
+
+for (var i = 0; i < classname.length; i++) {
+    classname[i].querySelector("a").removeAttribute('href');
+    classname[i].addEventListener('click', myFunction, false);
+
+
+}
---
+++ src/js/notes-images.js
@@ -0,0 +1,18 @@
+var classname = document.querySelectorAll(".noot");
+
+var myFunction = function() {
+    var attribute = this.querySelector("span").getAttribute("id");
+    attribute = attribute.replace("-backlink", "")
+    noteContent = document.getElementById(attribute).innerHTML;
+    document.getElementById("chapterIllustration").innerHTML= noteContent;
+
+    //alert(noteContent);
+
+};
+
+for (var i = 0; i < classname.length; i++) {
+    classname[i].querySelector("a").removeAttribute('href');
+    classname[i].addEventListener('click', myFunction, false);
+
+
+}
