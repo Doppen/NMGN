7a8272b4f013c653f1d0914a9476ac225b01401f
Bas Doppen <basdoppen@gmail.com>
2019-12-12 14:35:06 +0100

    Image viewer

--- _dist/js/main.js
+++ _dist/js/main.js
@@ -296,8 +296,8 @@ function zoom(e) {
   var zoomer = e.currentTarget;
   e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
   e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX;
-  x = offsetX / zoomer.offsetWidth * 100;
-  y = offsetY / zoomer.offsetHeight * 100;
+  x = offsetX / zoomer.offsetWidth * 150;
+  y = offsetY / zoomer.offsetHeight * 150;
   zoomer.style.backgroundPosition = x + '% ' + y + '%';
 }
 
