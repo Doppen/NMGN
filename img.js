ba45f0ac35832c9f6655d24005f0652875decb38
doppen <basdoppen@gmail.com>
2020-06-25 16:04:08 +0200

    new chapters

---
+++ img.js
@@ -0,0 +1,67 @@
+
+const fs = require('fs');
+const sharp = require('sharp');
+// https://sharp.pixelplumbing.com/api-resize
+
+// images
+const inputFolder  = './original-images/';
+const outputFolder = './src/images/';
+
+  // read folder
+  fs.readdir(inputFolder, function (err, files) {
+      //handling error
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
+      }
+
+      // go throught content
+      files.forEach(function (file) {
+        //console.log(file);
+         fs.stat(inputFolder+file, function(err, stats) {
+          if(typeof stats !== "undefined") {
+            var ifIsDir = stats.isDirectory();
+            //if file is folder
+            if (ifIsDir) {
+              var folderPath = inputFolder+file+'/';
+              resizeFiles(folderPath, 170, file);
+              resizeFiles(folderPath, 600, file);
+              resizeFiles(folderPath, 2000, file);
+            }
+          }
+         });
+      });
+  });
+
+
+function resizeFiles(folderPath, size, foldername) {
+  var folderExt = size;
+  if (size == 2000) {
+    folderExt = 'big';
+  }
+
+  var outFolderSize = outputFolder+foldername+'/'+foldername+'-'+folderExt+'/';
+  console.log(outFolderSize);
+
+
+  // create folder
+  fs.mkdir(outFolderSize, {recursive: true}, err => {
+  //fs.mkdir(outFolderSize, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Directory created successfully!');
+  });
+
+
+   fs.readdir(folderPath, function (err, files) {
+    files.forEach(function (file) {
+      if (file.slice(file.length - 4) != '.tif') {
+        sharp(folderPath+file)
+          .resize({ width: size })
+          .toFile(outFolderSize+file, function(err) {});
+      }
+
+
+    });
+   });
+}
